3a26e22b421e7827f66f9c66653e63b1963deffb
Josef <jdhamelink0@gmail.com>
2022-05-17 13:26:31 +0200

    Refactor
    Turns out it did not work as intended before, for obvious reasons.

--- main.py
+++ main.py
@@ -15,34 +15,34 @@ import time
 from itertools import product, combinations
 
 def main():
-    n_games = 10000
+    global N_ATTRIBUTES, N_VARIATIONS
+    N_ATTRIBUTES = 4
+    N_VARIATIONS = 3
+    n_games = 1000
 
+    highest = 0
+    highest_table = set()
+    set_in_highest = set()
+    
     tic = time.perf_counter()
+
     for i in range(n_games):
         try:
-            if (i)%(n_games//100) == 0:
-                print(f'\r{i}/{n_games}', end='')
+            if (i)%(n_games//100) == 0: print(f'\r{i}/{n_games}', end='')
         except ZeroDivisionError: print(f'\r{i}/{n_games}', end='')
-        game()
-    print('\r                          ', end='')
+        
+        highest, highest_table, set_in_highest = game(highest, highest_table, set_in_highest)
+    
     toc = time.perf_counter()
 
-    print(f'largest amount of cards on table: {HIGHEST}')
-    for card in H_TABLE:
-        if card in SET_IN_H: print(f'\033[36m{card}\033[0m')
+    print(f'\rlargest amount of cards on table: {highest}')
+    for card in highest_table:
+        if card in set_in_highest: print(f'\033[36m{card}\033[0m')
         else: print(card)
     print(f'{n_games} games took {toc-tic:.2f} seconds')
 
-def game():
-    global N_ATTRIBUTES, N_VARIATIONS
-    global HIGHEST, H_TABLE, SET_IN_H
-    
-    N_ATTRIBUTES = 4
-    N_VARIATIONS = 3
-    
-    HIGHEST = 0
-    H_TABLE = set()
-    SET_IN_H = set()
+def game(highest: int, highest_table: set[tuple], set_in_highest: set[tuple]) -> \
+            tuple[int,                set[tuple],                 set[tuple]]:
 
     pile = set(product(*[range(N_VARIATIONS) for _ in range(N_ATTRIBUTES)]))
     table = set()
@@ -53,12 +53,14 @@ def game():
             table.add(draw(pile))
         for candidate_set in combinations(table, N_VARIATIONS):
             if is_set(candidate_set):
-                if len(table) > HIGHEST: HIGHEST, H_TABLE, SET_IN_H = len(table), table.copy(), candidate_set
+                if len(table) > highest: highest, highest_table, set_in_highest = len(table), table.copy(), candidate_set
                 for card in candidate_set: table.remove(card)
                 break
         else:
             if not pile: break
             table.add(draw(pile))
+
+    return (highest, highest_table, set_in_highest)
 
 def draw(pile: set[tuple]) -> tuple:
     card = random.choice([*pile])
