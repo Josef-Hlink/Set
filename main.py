d7ef4c3cdfa24c50ab4dcd9b5dcfa98e4542043f
Josef <jdhamelink0@gmail.com>
2022-05-18 23:34:16 +0200

    Major speed boost, type hinting, and comments
    Also some namechanges, typo fixes and refactoring of the draw() function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Functional approach at creating the SET game
+Functional approach at solving the mathematics of SET
 ------------------------------------------------------------------
 This script can be configured to run any number of set games,
 with different numbers of attributes and variations.
@@ -10,66 +10,83 @@ Author: J.D. Hamelink
 Date: May 2022
 """
 
-import random
-import time
-from itertools import product, combinations
+import random                                   # taking random cards from deck
+import time                                     # tracking experiment time
+from itertools import product, combinations     # mathematical stuff I couldn't be bothered to write out
+from typing import TypeAlias                    # type hinting <3
+from functools import cache                     # major speed increase
+
+Card: TypeAlias = tuple[int]
 
 def main():
-    global N_ATTRIBUTES, N_VARIATIONS
-    N_ATTRIBUTES = 4
-    N_VARIATIONS = 3
-    n_games = 1000
-
-    highest = 0
-    highest_table = set()
-    set_in_highest = set()
+    # 1e3 = 1K, 1e5 = 100K, etc. this notation makes it easier to switch n_games 
+    n_games: int = 1e3
+    random.seed(123)
     
-    tic = time.perf_counter()
+    global N_ATTRIBUTES, N_VARIATIONS
+    N_ATTRIBUTES = 4        # for "normal" SET: number, color, shape, filling
+    N_VARIATIONS = 3        # for "normal" SET: 123,    gbr,   dwp,   ehf
+    
+    highest: int = 0                    # largest number of cards on table
+    highest_table: set[Card] = set()    # table where ^ was encountered
+    set_in_highest: set[Card] = set()   # valid set that was found in ^ table
+    
+    tic: float = time.perf_counter()
 
-    for i in range(n_games):
+    for i in range(int(n_games)):
+        # progress counter
         try:
-            if (i)%(n_games//100) == 0: print(f'\r{i}/{n_games}', end='')
+            if (i)%(n_games//100) == 0: print(f'\r{round(i/n_games*100)}%', end='')
         except ZeroDivisionError: print(f'\r{i}/{n_games}', end='')
-        
+        # one playout
         highest, highest_table, set_in_highest = game(highest, highest_table, set_in_highest)
     
-    toc = time.perf_counter()
+    toc: float = time.perf_counter()
 
-    print(f'\rlargest amount of cards on table: {highest}')
+    # printing statistics
+    print(f'\rlargest number of cards on table: {highest}')
     for card in highest_table:
-        if card in set_in_highest: print(f'\033[36m{card}\033[0m')
-        else: print(card)
-    print(f'{n_games} games took {toc-tic:.2f} seconds')
+        if card in set_in_highest: print(f'\033[1m\033[36m{card} <-\033[0m')
+        else:                      print(card)
+    print(f'{int(n_games)} games took {toc-tic:.2f} seconds')
 
-def game(highest: int, highest_table: set[tuple], set_in_highest: set[tuple]) -> \
-            tuple[int,                set[tuple],                 set[tuple]]:
+def game(highest: int, highest_table: set[Card], set_in_highest: set[Card]) -> tuple[int, set[Card], set[Card]]:
+    """Does one playout of a SET game where the computer finds all valid sets until deck is empty"""
 
-    pile = set(product(*[range(N_VARIATIONS) for _ in range(N_ATTRIBUTES)]))
-    table = set()
+    deck: set[Card] = set(product(*[range(N_VARIATIONS) for _ in range(N_ATTRIBUTES)]))
+    table: set[Card]  = set()
+
+    def draw() -> Card:
+        """Draws one random card from deck"""
+        card: Card = random.choice([*deck])
+        deck.remove(card)
+        return card
 
     while True:
+        # fill up table until minimum amount of cards are present
         while len(table) < N_VARIATIONS**2:
-            if not pile: break
-            table.add(draw(pile))
+            if not deck: break
+            table.add(draw())
+        # play one round; see if any sets are present on the table
         for candidate_set in combinations(table, N_VARIATIONS):
             if is_set(candidate_set):
-                if len(table) > highest: highest, highest_table, set_in_highest = len(table), table.copy(), candidate_set
+                if len(table) > highest:
+                    highest, highest_table, set_in_highest = len(table), table.copy(), candidate_set
                 for card in candidate_set: table.remove(card)
                 break
-        else:
-            if not pile: break
-            table.add(draw(pile))
+        # always add one card after a round
+        if not deck: break
+        table.add(draw())
 
     return (highest, highest_table, set_in_highest)
 
-def draw(pile: set[tuple]) -> tuple:
-    card = random.choice([*pile])
-    pile.remove(card)
-    return card
-
-def is_set(triple: set[tuple]) -> bool:
+@cache
+def is_set(candidate: set[Card]) -> bool:
+    """Checks if a triple of cards is a valid set"""
     for i in range(N_ATTRIBUTES):
-        if len({triple[j][i] for j in range(N_VARIATIONS)}) in range(2, N_VARIATIONS): return False
+        # attribute variations must either be identical or all unique
+        # if number of unique values is neither 1 nor n_variations, the candidate can not be valid
+        if len({candidate[j][i] for j in range(N_VARIATIONS)}) in range(2, N_VARIATIONS): return False
     return True
 
 
